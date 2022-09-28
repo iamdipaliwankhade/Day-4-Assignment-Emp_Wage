ef8489323ed7b3854d009be75fc15dddff851f9a
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-28 13:03:21 +0530

    [Dipali] : max monthly wage

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -20,7 +20,11 @@ public class EmployeeWage {
 		case 0:
 			System.out.println("Employee is present");
 			System.out.println("Employee wage=" + fullDayHour * wagePerHour);
-			System.out.println("Employee monthly wage=" + fullDayHour * wagePerHour * workingDayPerMonth);
+			int totalHourPerMonth = 1;
+			while (totalHourPerMonth <= 100) {
+				totalHourPerMonth++;
+			}
+			System.out.println("Employee monthly wage=" + totalHourPerMonth * wagePerHour);
 			break;
 
 		case 1:
@@ -33,6 +37,8 @@ public class EmployeeWage {
 			System.out.println("Employee is absent");
 			System.out.println("Employee wage=0");
 			break;
+
 		}
+
 	}
 }
