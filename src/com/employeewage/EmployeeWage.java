acb8d583cfdb153715499bec131fdd28c344d5c7
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-28 12:58:44 +0530

    [Dipali] : using switch case

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -8,22 +8,30 @@ package com.employeewage;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation program");
-
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int partTimeHour = 4;
 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME) {
+		int isPresent = (int) Math.floor(Math.random() * 3);
+		switch (isPresent) {
+		case 0:
 			System.out.println("Employee is present");
-			System.out.println("Employee daily wage=" + fullDayHour * wagePerHour);
-		} else if (empCheck == 0) {
+			System.out.println("Employee wage=" + fullDayHour * wagePerHour);
+			break;
+
+		case 1:
+			System.out.println("Employee is present as a part time");
+			System.out.println("Employee wage=" + partTimeHour * wagePerHour);
+			break;
+
+		case 2:
 			System.out.println("Employee is absent");
-		} else {
-			System.out.println("Employee working as a part time");
-			System.out.println("Employee part time wage :" + partTimeHour * wagePerHour);
+			System.out.println("Employee wage=0");
+			break;
 		}
+
 	}
 
 }
