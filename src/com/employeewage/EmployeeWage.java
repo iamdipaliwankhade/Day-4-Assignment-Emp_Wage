40d56d33e7978037bf45cf960f59e552d64ffbfa
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-28 12:50:58 +0530

    [Dipali] : employee attendance

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -6,8 +6,19 @@ package com.employeewage;
  */
 public class EmployeeWage {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("Welcome to Employee Wage Computation Program.");
+
+		int IS_FULL_TIME = 1;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == IS_FULL_TIME)
+
+			System.out.println("Employee is Present");
+		else
+
+			System.out.println("Employee is Absent");
+
 	}
 }
